cca2f7fdeff548f73385486f6a8d9fa3d2537b5f
Cong Tai <congtai836@mgmail.com>
2023-07-13 22:33:59 +0700

    first commit

---
+++ app/src/main/java/com/example/teramall/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.teramall.Adapter;
+
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.teramall.Fragment.AccountFragment;
+import com.example.teramall.Fragment.ShopFragment;
+import com.example.teramall.Fragment.TeraFragment;
+import com.example.teramall.Fragment.UnknowFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new TeraFragment();
+            case 1:
+                return new UnknowFragment();
+            case 2:
+                return new ShopFragment();
+            case 3:
+                return new AccountFragment();
+            default:
+                return new TeraFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
